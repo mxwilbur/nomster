aadab0aa90b32d61a60df7d9dde1e5c95fa3fdf8
mxwilbur <mxwilbur@outlook.com>
2019-06-01 21:53:38 +0000

    message

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @placelvalid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
