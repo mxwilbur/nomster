15d76f5076e7d7db0f0cc2d11cf8f922f3ba0275
mxwilbur <mxwilbur@outlook.com>
2019-06-01 20:46:27 +0000

    bug fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   def index
     @pagy, @places = pagy(Place.all, items: 10)
 
@@ -20,6 +20,10 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: "Not Allowed", status: :forbidden
+    end
   end
 
   def update
