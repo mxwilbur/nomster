1efc50610fef29412ea7b8d6fc0a8754c482bfee
mxwilbur <mxwilbur@outlook.com>
2019-06-05 00:27:16 +0000

    places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
-    @pagy, @places = pagy(Place.all, items: 10)
+    @pagy, @places = pagy(Place.all, items: 5)
 
   end
 
