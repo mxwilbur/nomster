623447725cbf777f8af631e96b11708bd73244ae
mxwilbur <mxwilbur@outlook.com>
2019-06-05 13:41:53 +0000

    comment db + model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_181524) do
+ActiveRecord::Schema.define(version: 2019_06_05_131023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
